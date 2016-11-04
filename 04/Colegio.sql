a24224d2459dc1335e65ce323d05db97d1747899
forameuss <navarrogordilloalberto@gmail.com>
2016-11-04 09:19:51 +0100

    comenzado ejercicio biblioteca

---
+++ 04/Colegio.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.2
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 04-11-2016 a las 08:30:57
+-- Versión del servidor: 10.1.16-MariaDB
+-- Versión de PHP: 7.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `Colegio`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Alumnos`
+--
+
+CREATE TABLE `Alumnos` (
+  `id` int(6) UNSIGNED NOT NULL,
+  `nombre` varchar(30) CHARACTER SET latin1 NOT NULL,
+  `apellidos` varchar(30) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `Alumnos`
+--
+
+INSERT INTO `Alumnos` (`id`, `nombre`, `apellidos`) VALUES
+(1, 'Rafael', 'Alberti'),
+(6, ' Ãlvaro ', ' Ojeda'),
+(7, ' Gustavo ', ' GarcÃ­a');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `Alumnos`
+--
+ALTER TABLE `Alumnos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `Alumnos`
+--
+ALTER TABLE `Alumnos`
+  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
